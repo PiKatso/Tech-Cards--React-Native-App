188c460a3e95f471c2a4311aa58aef0b728e908c
Kat Gifford <katgiff2007@gmail.com>
2018-03-18 16:44:54 -0700

    pass the action creator a param/payload of libraryId

--- src/Actions/index.js
+++ src/Actions/index.js
@@ -1,6 +1,7 @@
-export const selectLibrary = () => {
+export const selectLibrary = (libraryID) => {
   // creates and object with type property/action
   return {
-    type: 'select_library'
+    type: 'select_library',
+    payload: libraryId
   };
 }; // this wrapper/function is an action creator
