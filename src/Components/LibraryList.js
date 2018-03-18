fdac4e25260cf7ebb48fdd94af13db70dd627162
Kat Gifford <katgiff2007@gmail.com>
2018-03-18 15:48:49 -0700

    finish set up of the connect helper and begin implementation of the ListView Component type to handle viewing the data

--- src/Components/LibraryList.js
+++ src/Components/LibraryList.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
+import { ListView } from 'react-native';
 import { connect } from 'react-redux';
 
 class LibraryList extends Component {
+  componentWillMount() {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+
+    //the ds object uses the props from the MapStateToProps funct
+    this.dataSource = ds.cloneWithRows(this.props.libraries);
+  } //
+
   render() {
-    return;
+    return(
+      <ListView
+        dataSource={this.dataSource}
+      />
+    );
   }
 }
 
 const mapStateToProps = state => {
-  console.log(state);
+  return { libraries: state.libraries }; //libraries to show
 };
 
 export default connect(mapStateToProps)(LibraryList);
