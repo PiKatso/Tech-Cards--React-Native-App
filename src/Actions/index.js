3c053e8a3f9d255604c0546d2bc5d7b95c63de10
Kat Gifford <katgiff2007@gmail.com>
2018-03-18 16:43:53 -0700

    create an action creator

---
+++ src/Actions/index.js
@@ -0,0 +1,6 @@
+export const selectLibrary = () => {
+  // creates and object with type property/action
+  return {
+    type: 'select_library'
+  };
+}; // this wrapper/function is an action creator
